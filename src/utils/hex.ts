38b3ec83298ed821de0ce35d0bc39026107babcd
apademide <contact@apademide.ch>
2025-03-06 16:09:42 +0100

    feat: `isHex`+`parseHex`

---
+++ src/utils/hex.ts
@@ -0,0 +1,48 @@
+import { S } from "@auaust/primitive-kit";
+import type { ColorComponents, Hex } from "~/types";
+
+const hexadecimalRegex = /^[0-9a-f]+$/i;
+
+/**
+ * Whether the input is a valid HEX color. With or without the alpha channel, and with single or double digits.
+ *
+ * It may or may not start with a hash character, which will be ignored.
+ */
+export function isHex(value: unknown): value is Hex {
+  value = S.is(value) && (value.startsWith("#") ? value.slice(1) : value);
+
+  if (!S.isStrict(value)) {
+    return false;
+  }
+
+  switch (value.length) {
+    case 3: // RGB
+    case 4: // RGBA
+    case 6: // RRGGBB
+    case 8: // RRGGBBAA
+      return hexadecimalRegex.test(value);
+    default:
+      return false;
+  }
+}
+
+/**
+ * Parses a hex string into an object of color channels.
+ *
+ * The input must already be a valid HEX color, otherwise the result will be unexpected.
+ */
+export function parseHex(value: Hex): ColorComponents {
+  value = value.startsWith("#") ? value.slice(1) : value;
+
+  const isShort = value.length < 6;
+  const hasAlpha = isShort ? value.length === 4 : value.length === 8;
+
+  const r = parseInt(isShort ? value[0].repeat(2) : value.slice(0, 2), 16);
+  const g = parseInt(isShort ? value[1].repeat(2) : value.slice(2, 4), 16);
+  const b = parseInt(isShort ? value[2].repeat(2) : value.slice(4, 6), 16);
+  const a = hasAlpha
+    ? parseInt(isShort ? value[3].repeat(2) : value.slice(6, 8), 16) / 255
+    : 1;
+
+  return { r, g, b, a };
+}
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,14 +1,58 @@
 import { describe, expect, test } from "vitest";
 
-import index from "~";
-import utils from "~/utils";
+import { isHex, parseHex } from "~/utils/hex";
 
-describe("index", () => {
-  test("index", () => {
-    expect(index).toBe("index");
+describe("HEX colors", () => {
+  test("can be recognized", () => {
+    const hex = [
+      "#c85",
+      "3f9",
+      "#810e",
+      "#deb6f8",
+      "#149d15f4",
+      "aBcDeF01",
+      "#12345678",
+      "abcdef",
+    ];
+
+    hex.forEach((h) => {
+      expect(isHex(h)).toBe(true);
+    });
+
+    const notHex = [
+      "#xyz",
+      "#12345g",
+      "#123456789",
+      "#12",
+      {},
+      NaN,
+      undefined,
+      1,
+      "",
+    ];
+
+    notHex.forEach((h) => {
+      expect(isHex(h)).toBe(false);
+    });
   });
 
-  test("utils", () => {
-    expect(utils).toBe("utils/index");
+  test("can be parsed", () => {
+    const hex = {
+      c85: { r: 204, g: 136, b: 85, a: 1 },
+      "#810e": { r: 136, g: 17, b: 0, a: (1 / 16) * 15 },
+      deb6f8: { r: 222, g: 182, b: 248, a: 1 },
+      "#149d15f4": { r: 20, g: 157, b: 21, a: (1 / 256) * 244 },
+      aBcDeF01: { r: 171, g: 205, b: 239, a: (1 / 256) * 1 },
+      "#12345678": { r: 18, g: 52, b: 86, a: (1 / 256) * 120 },
+    };
+
+    Object.entries(hex).forEach(([input, expected]) => {
+      const components = parseHex(input);
+
+      expect(components.r).toBe(expected.r);
+      expect(components.g).toBe(expected.g);
+      expect(components.b).toBe(expected.b);
+      expect(components.a).toBeCloseTo(expected.a);
+    });
   });
 });
