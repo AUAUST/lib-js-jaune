af54d041e8484dda2a863ab5b4f92249e5a8bb28
apademide <contact@apademide.ch>
2025-12-30 01:06:41 +0100

    meta: allow code splitting
    Avoid duplication of helpers across entries

--- tsup.config.ts
+++ tsup.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   format: ["esm", "cjs"],
   outDir: "dist",
   dts: true,
-  splitting: false,
+  splitting: true,
   sourcemap: true,
   clean: false,
   minify: true,
